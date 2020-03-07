3724bdaa16617f67db57253db16ea1d627302a22
Tammer Kamel <tammer@ilukacg.com>
2020-03-07 07:57:30 -0500

    init base.html and CSS

--- mysite/settings.py
+++ mysite/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # 'users.apps.Users.Config',
     'pool.apps.PoolConfig',
     'django.contrib.admin',
     'django.contrib.auth',
