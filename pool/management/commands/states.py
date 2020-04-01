c57b4459893249c0586b6017f824bfeb37dd70ea
Tammer Kamel <tammer@ilukacg.com>
2020-04-01 13:25:17 -0400

    -

--- pool/management/commands/states.py
+++ pool/management/commands/states.py
@@ -1,11 +1,11 @@
 from django.core.management.base import BaseCommand, CommandError
-from pool.models import Team,Game
+from pool.models import Team,Game,Pick
 import random
 from datetime import datetime, timedelta
 
 dates = [
 	# (2019,8,18,13,21,0), # Preseason
-	(2019,10,18,13,21,0), # Friday 13:21
+	# (2019,10,18,13,21,0), # Friday 13:21
 	# (2019,10,20,13,21,0), # Sunday 13:21
 	# (2019,10,21,13,21,0), # Monday 13:21
 	# (2019,10,22,13,21,0), # Tuesday 13:21
@@ -15,12 +15,19 @@ dates = [
 	(2019,12,21,20,21,0), # Thursday 20:21
 ]
 
+def give_picks(week_number):
+	for pick in Pick.objects.filter(week_number=week_number):
+		pick.picked_fav = random.choice([True,False])
+		pick.save(force=True)
+
 def set_state(date):
 	# fill in spreads and scores
 	random.seed(1)
 	spread_week = 1
+	give_picks(spread_week)
 	while date > (Game.objects.filter(week_number=spread_week).order_by('game_number').first().game_date - timedelta(hours=60)):
 		spread_week +=1
+		give_picks(spread_week)
 	for game in Game.objects.all().order_by('game_date'):
 		if( game.week_number <= spread_week ):
 			game.spread = random.choice([0,1,1,2,2,2,3,3,3,3,4,4,5,6,6,7,9,10])
@@ -31,6 +38,8 @@ def set_state(date):
 				game.fav_score = None
 				game.udog_score = None
 		else:
+			game.fav_score = None
+			game.udog_score = None
 			game.spread = None
 		game.save()
 
