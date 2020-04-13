fe71c6c92acc52b0ee7f514be9dbe7154824f80c
Tammer Kamel <tammer@ilukacg.com>
2020-04-12 21:15:48 -0400

    -

--- mysite/settings.py
+++ mysite/settings.py
@@ -160,7 +160,7 @@ if config.production:
     EMAIL_PORT = config.EMAIL_PORT
     EMAIL_HOST_USER = config.EMAIL_HOST_USER
     EMAIL_HOST_PASSWORD = config.EMAIL_HOST_PASSWORD
-    EMAIL_USE_TLS = True
+    EMAIL_USE_SSL = True
     DEFAULT_FROM_EMAIL = 'Alpha Ape <pool@tammer.com>'
 else:
     EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
