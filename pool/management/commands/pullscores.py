4242f3d1b6b8a4a8da4f57d6cd4e0da8eeba0e71
Tammer Kamel <tammer@ilukacg.com>
2020-09-10 20:03:02 -0400

    -

--- pool/management/commands/pullscores.py
+++ pool/management/commands/pullscores.py
@@ -2,16 +2,21 @@ from django.core.management.base import BaseCommand, CommandError
 from pool.models import Game,now
 from pool.utils import load_scores,implied_week
 from datetime import datetime, timedelta
+from time import sleep
 
 class Command(BaseCommand):
 	help = 'Pull the latest scores'
 
 	def handle(self, *args, **options):
 		this_week = implied_week()
-		for g in Game.objects.filter(week_number=this_week):
-			expected_end_time = g.game_date + timedelta(hours=2,minutes=45)
-			if not(g.isOver()) and now() > expected_end_time:
-				print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": CHECKING FOR NEW FINAL SCORES")
-				load_scores()
-				return
-		print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": No games need to be updated at this time")
+		MAX = 3
+		for i in range(MAX):
+			for g in Game.objects.filter(week_number=this_week):
+				expected_end_time = g.game_date + timedelta(hours=2,minutes=45)
+				if not(g.isOver()) and now() > expected_end_time:
+					print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": CHECKING FOR NEW FINAL SCORES")
+					load_scores()
+					break
+			print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": No games need to be updated at this time")
+			if i < MAX-1:
+				sleep(300)
