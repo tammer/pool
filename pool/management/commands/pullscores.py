21cedd2c71c9a580a3163c21de65e1bbc192c8a7
Tammer Kamel <tammer@ilukacg.com>
2020-09-11 07:38:03 -0400

    -

--- pool/management/commands/pullscores.py
+++ pool/management/commands/pullscores.py
@@ -11,12 +11,15 @@ class Command(BaseCommand):
 		this_week = implied_week()
 		MAX = 3
 		for i in range(MAX):
+			scores_have_been_loaded = False
 			for g in Game.objects.filter(week_number=this_week):
 				expected_end_time = g.game_date + timedelta(hours=2,minutes=45)
 				if not(g.isOver()) and now() > expected_end_time:
 					print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": CHECKING FOR NEW FINAL SCORES")
 					load_scores()
+					scores_have_been_loaded = True
 					break
-			print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": No games need to be updated at this time")
+			if not(scores_have_been_loaded):
+				print(now().strftime("%Y-%m-%d, %H:%M:%S") + ": No games need to be updated at this time")
 			if i < MAX-1:
 				sleep(300)
